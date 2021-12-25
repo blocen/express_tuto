d052f647873767bcb88f40a8d2f78de495b44001
Raphael <89421168+blocen@users.noreply.github.com>
2021-12-25 12:58:53 +0100

    render home, userRouter

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-import express from "express";
+import express from 'express';
 import { morganMiddleware } from "./middleware/morgan.js";
 
 const app = express();  
 app.use(express.json());
 app.use(morganMiddleware);
+app.set('view engine', 'ejs');
 const port = 3000;
 
 app.get("/", (req, resp) =>  {
-  resp.send("hello world...");
+  // resp.send("hello world...");
+  // resp.status(200).send("hello world...");
+  resp.status(200).json({message: "some message"});
+  // resp.download("home.html");
 });
+
+app.get("/home", (req, resp) =>  {
+  resp.render("home", { text: "more data" });
+});
+
+// const userRouter = require('./routes/users');
+import router from './routes/users.cjs';
+const userRouter = router;
+app.use('/users', userRouter);
+
+// import router from './routes/some.cjs';
+// const someRouter = router;
+// app.use('/some', someRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// import express from 'express'
+const router = express.Router();
+
+
+
+router.get("/", (req, resp) =>  {
+  resp.send("user list");
+});
+
+router.get("/new", (req, resp) =>  {
+  resp.send("user new form");
+});
+
+// module.exports = router;
+export { router };
