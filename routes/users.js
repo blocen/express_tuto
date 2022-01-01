67513057361b0801cd143b04596c80c62c26d1f3
Raphael <89421168+blocen@users.noreply.github.com>
2022-01-01 23:30:49 +0100

    fix imports/exports

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const express = require('express');
-// import express from 'express'
-const router = express.Router();
+// const express = require('express');
+import express from 'express'
+export const router = express.Router();
 
 
 
@@ -13,4 +13,4 @@ router.get("/new", (req, resp) =>  {
 });
 
 // module.exports = router;
-export { router };
+export default router;
