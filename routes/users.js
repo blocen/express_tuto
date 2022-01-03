7d28b848e8d8e3d4f70f75b6cb0fdba37a5a5990
Raphael <89421168+blocen@users.noreply.github.com>
2022-01-03 23:08:53 +0100

    cleanup

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,5 @@
-// const express = require('express');
 import express from 'express'
 export const router = express.Router();
-
-
 
 router.get("/", (req, resp) =>  {
   resp.send("user list");
@@ -12,5 +9,4 @@ router.get("/new", (req, resp) =>  {
   resp.send("user new form");
 });
 
-// module.exports = router;
 export default router;
