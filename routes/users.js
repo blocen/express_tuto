3b78f3a396e8d5b5e8c0f2862123389747e20db1
Raphael <89421168+blocen@users.noreply.github.com>
2022-01-04 12:53:36 +0100

    router.route()

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,35 @@ router.get("/new", (req, resp) =>  {
   resp.send("user new form");
 });
 
+// router.get("/:id", (req, resp) =>  {
+//   let id = req.params.id;
+//   resp.send(`user get with id: ${id}`);
+// });
+
+// router.put("/:id", (req, resp) =>  {
+//   let id = req.params.id;
+//   resp.send(`user get with id: ${id}`);
+// });
+
+// router.delete("/:id", (req, resp) =>  {
+//   let id = req.params.id;
+//   resp.send(`user get with id: ${id}`);
+// });
+
+router
+  .route("/:id")
+  .get((req, resp) =>  {
+    resp.send(`user get with id: ${req.params.id}`)
+  })
+  .put((req, resp) =>  {
+    resp.send(`user update/put with id: ${req.params.id}`)
+  })
+  .delete((req, resp) =>  {
+    resp.send(`user delete with id: ${req.params.id}`)
+  });
+
+router.post("/", (req, resp) =>  {
+  resp.send("create user");
+});
+
 export default router;
